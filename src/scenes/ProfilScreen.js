578b757df86fb60776b8b210ed2f824bf746710c
Shun-Lassal <lassal.shun@gmail.com>
2022-04-01 16:17:04 +0200

    push

--- src/scenes/ProfilScreen.js
+++ src/scenes/ProfilScreen.js
@@ -2,10 +2,22 @@ const ProfilScreen = ({ route, navigation }) => {
     const { pseudo, password } = route.params;
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text style={styles}>Nom du profil: {pseudo}</Text>
-            <Text style={styles}>Password: {password}</Text>
-            <Text style={styles}>Parties Jouées</Text>
-            <Text style={styles}>Parties Gagnées</Text>
+            <View style={styles}>
+                <Image/>
+                <Text style={styles}>uuu</Text>
+            </View>
+            <View style={styles}>
+                <Text style={styles}>A propos de moi:</Text>
+                <View style={styles}>
+                    <Text style={styles}>'uu'u'u'u'u'u'uuuuuuuuu'uuu'u'uu'u'u'</Text>
+                </View>
+            </View>
+            <View style={styles}>
+                <Text style={styles}>{pseudo}</Text>
+                <Text style={styles}>Date de création du compte</Text>
+                <Text style={styles}>Parties Jouées: </Text>
+                <Text style={styles}>Parties Gagnées: </Text>
+            </View>
             <Button
                 style={styles.buttonRegisterLogin}
                 title="Retour au menu"
